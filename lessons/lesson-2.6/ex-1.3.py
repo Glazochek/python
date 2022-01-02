23c0b2c5c50b8c9bffbd167452983d6d3d9ae5f1
GlaZocheK <an9680411@gmail.com>
2022-01-02 22:51:33 +0300

    дз на 6 урок

---
+++ lessons/lesson-2.6/ex-1.3.py
@@ -0,0 +1,21 @@
+from pympler import asizeof
+# Основы языка Python: урок 1 задание 2
+
+
+def tg():
+    number_list = []
+    for i in range(0, 10000):
+        if i % 2 != 0:
+            i = i ** 3
+            number_list.append(i)
+    return number_list
+
+
+def tg1():
+    return (i**3 for i in range(0, 10000) if i % 2 != 0)
+
+
+print(asizeof.asizeof(tg()))  # 201880
+print(asizeof.asizeof(tg1()))  # 432
+# поразительно, поменял list на картеж и вот результат
+# так же использовал кортежное включение
---
+++ lessons/lesson-2.6/ex-1.5.py
@@ -0,0 +1,32 @@
+from pympler import asizeof
+
+# Алгоритмы и структуры данных на Python: урок 4 задание 3
+
+array = [1, 3, 3, 2, 4, 5, 2, 1, 3, 3]
+
+
+# было
+def func_1():
+    new_array = []
+    for el in array:
+        count2 = array.count(el)
+        new_array.append(count2)
+
+    mx = max(new_array)
+    elem = array[new_array.index(mx)]
+    return f'Чаще всего встречается число {elem}, ' \
+           f'оно появилось в массиве {mx} раз(а)'
+
+
+# стало
+def func_2():
+    for n in tuple((el, array.count(el)) for el in array):
+        if n[1] == max(map(array.count, array)):
+            return f'Чаще встречается {n[0]} оно появилось {n[1]} раз'
+
+
+print(asizeof.asizeof(func_1()))  # 208
+print(asizeof.asizeof(func_2()))  # 152
+
+#  столько всего сделал
+#  например сделал картежное включение и воспользовался функцией map
